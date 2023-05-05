2ab19df8eb0c03648d21f6e00ead8cd36f6b3683
ErikBoesen <me@erikboesen.com>
2023-05-05 16:03:28 -0400

    Add code to persist cookies

--- main.js
+++ main.js
@@ -8,9 +8,34 @@ function createWindow () {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       icon: path.join(__dirname, 'images/logo/logo.png'),
+      partition: 'persist:infragistics',
     },
   })
 
+  // Persist cookies
+  let cookies = win.webContents.session.cookies;
+  cookies.on('changed', function(event, cookie, cause, removed) {
+    if (cookie.session && !removed) {
+      let url = util.format('%s://%s%s', (!cookie.httpOnly && cookie.secure) ? 'https' : 'http', cookie.domain, cookie.path);
+      console.log('url', url);
+      cookies.set({
+        url: url,
+        name: cookie.name,
+        value: cookie.value,
+        domain: cookie.domain,
+        path: cookie.path,
+        secure: cookie.secure,
+        httpOnly: cookie.httpOnly,
+        expirationDate: new Date().setDate(new Date().getDate() + 14)
+      }, function(err) {
+        if (err) {
+          log.error('Error trying to persist cookie', err, cookie);
+        }
+      });
+    }
+  });
+
+  // Load webpage
   win.loadURL(
       'https://web.groupme.com',
       {
